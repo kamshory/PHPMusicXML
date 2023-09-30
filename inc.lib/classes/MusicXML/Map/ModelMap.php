5729cfcc345d15733f31d15cfb4d3b0f2abd0c6c
Kamshory, MT <kamshory@yahoo.com>
2023-10-01 05:46:26 +0700

    Update

--- inc.lib/classes/MusicXML/Map/ModelMap.php
+++ inc.lib/classes/MusicXML/Map/ModelMap.php
@@ -7,4 +7,11 @@ class ModelMap
     const CLASS_MAP = array(
         "score-partwise"=>"\\MusicXML\\Model\\ScorePartWise"
     );
+
+    const PROPERTY_MAP = array(
+        "direction-type"=>"directionType",
+        "encoding-date"=>"encodingDate",
+        "software"=>"softwareList",
+        "rights"=>"copyrights"
+    );
 }
