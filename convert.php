bbea4a4943880c82b5336e280a6866b3eaf641c7
Kamshory, MT <kamshory@yahoo.com>
2023-10-04 00:07:01 +0700

    Update

--- convert.php
+++ convert.php
@@ -8,11 +8,11 @@ require_once "inc.lib/autoload.php";
 $musicXML = new MusicXML();
 try
 {
-    $midi = $musicXML->loadMidi("sherina.mid");
+    $midi = $musicXML->loadMidi("test.mid");
     $mxl = new MXL();
     $xml = $musicXML->midiToMusicXml($midi, "Cinta Pertama dan Terakhir", "4.0", "xml");
     //file_put_contents("convert.mxl", $mxl->createMxl("Cinta Pertama dan Terakhir", $xml));
-    file_put_contents("convert.xml", $xml);
+    file_put_contents("test.xml", $xml);
 }
 catch(Exception $e)
 {
