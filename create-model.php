6f1e62886cae270b048ff99b86f995a1a09668aa
Kamshory, MT <kamshory@yahoo.com>
2023-10-06 18:03:46 +0700

    Add model from autogenerated

---
+++ create-model.php
@@ -0,0 +1,119 @@
+<?php
+$url = 'https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/hammer-on/';
+
+libxml_use_internal_errors(true);
+
+
+
+
+function url_check($url)
+{
+    $headers = @get_headers($url);
+    return is_array($headers) ? preg_match('/^HTTP\\/\\d+\\.\\d+\\s+2\\d\\d\\s+.*$/', $headers[0]) : false;
+}
+
+function clean($text)
+{
+    return html_entity_decode(trim(str_replace(';', '-', preg_replace('/\s+/S', " ", strip_tags($text))))); // remove everything
+}
+
+$name = basename($url);
+
+$className = str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+
+$path = __DIR__.'/inc.lib/classes/MusicXML/Model/'.$className.".php";
+if (!file_exists($path)) {
+    echo "FILE NOT EXISTS\r\n";
+
+    $attributes = array();
+    if (url_check($url)) {
+        $doc = new DomDocument;
+        $doc->validateOnParse = true;
+        $doc->loadHtml(file_get_contents($url));
+
+        $containers = $doc->getElementsByTagName('main');
+        $tableIndex = 0;
+        $rowIndex = 0;
+        foreach ($containers as $container2) {
+
+            foreach ($container2->getElementsByTagName('table') as $container3) {
+                foreach ($container3->getElementsByTagName('tr') as $tr) {
+
+                    if ($rowIndex > 0) {
+                        $cells1 = $tr->getElementsByTagName('th');
+                        $cells2 = $tr->getElementsByTagName('td');
+                        $name = $cells1->item(0)->nodeValue;
+                        $value = $cells2->item(1)->firstChild->nodeValue;
+                        $attributes[] = array('name'=>$name, 'value'=>$value);
+                    }
+
+                    $rowIndex++;
+                }
+            }
+            $tableIndex++;
+        }
+    } else {
+        echo 'URL not reachable!'; // Throw message when URL not be called
+    }
+
+    $attrs = array();
+
+    
+    foreach ($attributes as $attribute) {
+        $attrs[] = createAttribute($attribute['name'], $attribute['value']);
+    }
+    
+    $template = '<?php
+
+namespace MusicXML\Model;
+
+use MusicXML\MusicXMLWriter;
+
+/**
+ * '.$className.'
+ * @Xml
+ * @Reference '.$url.'
+ * @Data
+ */
+class '.$className.' extends MusicXMLWriter
+{
+'.implode("\r\n", $attrs).'    
+}';
+file_put_contents($path, $template);
+
+}
+else
+{
+    echo "FILE EXISTS\r\n";
+}
+
+
+function createAttribute($name, $value)
+{
+    $type = getAttrType($name);
+    $attributeName = lcfirst(str_replace(' ', '', ucwords(str_replace('-', ' ', $name))));
+    $attribute = "\t/**
+\t * ".ucfirst(str_replace('-', ' ', $name))."
+\t *
+\t * @Attribute(name=\"$name\")
+\t * @var $type
+\t */
+\tpublic \$$attributeName;
+";
+    return $attribute;
+}
+
+function getAttrType($name)
+{
+    $types = array(
+        'dash-length'=>'float',
+        'default-x'=>'float',
+        'default-y'=>'float',
+        'relative-x'=>'float',
+        'relative-y'=>'float',
+        'first-beat'=>'float',
+        'space-length'=>'float'
+    );
+    
+    return isset($types[$name])?$types[$name]:'string';
+}
---
+++ inc.lib/classes/MusicXML/Model/Fingering.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace MusicXML\Model;
+
+use MusicXML\MusicXMLWriter;
+
+/**
+ * Fingering
+ * @Xml
+ * @Reference https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/fingering/
+ * @Data
+ */
+class Fingering extends MusicXMLWriter
+{
+	/**
+	 * Alternate
+	 *
+	 * @Attribute(name="alternate")
+	 * @var string
+	 */
+	public $alternate;
+
+	/**
+	 * Color
+	 *
+	 * @Attribute(name="color")
+	 * @var string
+	 */
+	public $color;
+
+	/**
+	 * Default x
+	 *
+	 * @Attribute(name="default-x")
+	 * @var float
+	 */
+	public $defaultX;
+
+	/**
+	 * Default y
+	 *
+	 * @Attribute(name="default-y")
+	 * @var float
+	 */
+	public $defaultY;
+
+	/**
+	 * Font family
+	 *
+	 * @Attribute(name="font-family")
+	 * @var string
+	 */
+	public $fontFamily;
+
+	/**
+	 * Font size
+	 *
+	 * @Attribute(name="font-size")
+	 * @var string
+	 */
+	public $fontSize;
+
+	/**
+	 * Font style
+	 *
+	 * @Attribute(name="font-style")
+	 * @var string
+	 */
+	public $fontStyle;
+
+	/**
+	 * Font weight
+	 *
+	 * @Attribute(name="font-weight")
+	 * @var string
+	 */
+	public $fontWeight;
+
+	/**
+	 * Placement
+	 *
+	 * @Attribute(name="placement")
+	 * @var string
+	 */
+	public $placement;
+
+	/**
+	 * Relative x
+	 *
+	 * @Attribute(name="relative-x")
+	 * @var float
+	 */
+	public $relativeX;
+
+	/**
+	 * Relative y
+	 *
+	 * @Attribute(name="relative-y")
+	 * @var float
+	 */
+	public $relativeY;
+
+	/**
+	 * Substitution
+	 *
+	 * @Attribute(name="substitution")
+	 * @var string
+	 */
+	public $substitution;
+	
+	/**
+	 * Text content
+	 *
+	 * @TextContent
+	 * @var string
+	 */
+	public $textContent;
+    
+}
