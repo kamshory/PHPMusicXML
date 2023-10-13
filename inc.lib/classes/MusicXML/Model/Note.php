28b5b2cce0785263028ce90c686874451773a8e0
Kamshory, MT <kamshory@yahoo.com>
2023-10-13 13:59:14 +0700

    Update note

--- inc.lib/classes/MusicXML/Model/Note.php
+++ inc.lib/classes/MusicXML/Model/Note.php
@@ -8,35 +8,178 @@ use MusicXML\MusicXMLWriter;
  * Note
  * @Xml
  * @MusicXML
- * @Path /path/measure/note
  * @Reference https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/note/
  * @Data
  */
 class Note extends MusicXMLWriter
 {
-    /**
-     * Default X
-     * 
-     * @Attribute(name="default-x") 
-     * @var float
-     */
-    public $defaultX;
-    
-    /**
-     * Default Y
-     * 
-     * @Attribute(name="default-y") 
-     * @var float
-     */
-    public $defaultY;
-    
-    /**
-     * Dynamics
-     *
-     * @Attribute
-     * @var float
-     */
-    public $dynamics;
+	/**
+	 * Attack
+	 *
+	 * @Attribute(name="attack")
+	 * @var string
+	 */
+	public $attack;
+
+	/**
+	 * Color
+	 *
+	 * @Attribute(name="color")
+	 * @var string
+	 */
+	public $color;
+
+	/**
+	 * Default x
+	 *
+	 * @Attribute(name="default-x")
+	 * @var float
+	 */
+	public $defaultX;
+
+	/**
+	 * Default y
+	 *
+	 * @Attribute(name="default-y")
+	 * @var float
+	 */
+	public $defaultY;
+
+	/**
+	 * Dynamics
+	 *
+	 * @Attribute(name="dynamics")
+	 * @var string
+	 */
+	public $dynamics;
+
+	/**
+	 * End dynamics
+	 *
+	 * @Attribute(name="end-dynamics")
+	 * @var string
+	 */
+	public $endDynamics;
+
+	/**
+	 * Font family
+	 *
+	 * @Attribute(name="font-family")
+	 * @var string
+	 */
+	public $fontFamily;
+
+	/**
+	 * Font size
+	 *
+	 * @Attribute(name="font-size")
+	 * @var string
+	 */
+	public $fontSize;
+
+	/**
+	 * Font style
+	 *
+	 * @Attribute(name="font-style")
+	 * @var string
+	 */
+	public $fontStyle;
+
+	/**
+	 * Font weight
+	 *
+	 * @Attribute(name="font-weight")
+	 * @var string
+	 */
+	public $fontWeight;
+
+	/**
+	 * Id
+	 *
+	 * @Attribute(name="id")
+	 * @var string
+	 */
+	public $id;
+
+	/**
+	 * Pizzicato
+	 *
+	 * @Attribute(name="pizzicato")
+	 * @var string
+	 */
+	public $pizzicato;
+
+	/**
+	 * Print dot
+	 *
+	 * @Attribute(name="print-dot")
+	 * @var string
+	 */
+	public $printDot;
+
+	/**
+	 * Print leger
+	 *
+	 * @Attribute(name="print-leger")
+	 * @var string
+	 */
+	public $printLeger;
+
+	/**
+	 * Print lyric
+	 *
+	 * @Attribute(name="print-lyric")
+	 * @var string
+	 */
+	public $printLyric;
+
+	/**
+	 * Print object
+	 *
+	 * @Attribute(name="print-object")
+	 * @var string
+	 */
+	public $printObject;
+
+	/**
+	 * Print spacing
+	 *
+	 * @Attribute(name="print-spacing")
+	 * @var string
+	 */
+	public $printSpacing;
+
+	/**
+	 * Relative x
+	 *
+	 * @Attribute(name="relative-x")
+	 * @var float
+	 */
+	public $relativeX;
+
+	/**
+	 * Relative y
+	 *
+	 * @Attribute(name="relative-y")
+	 * @var float
+	 */
+	public $relativeY;
+
+	/**
+	 * Release
+	 *
+	 * @Attribute(name="release")
+	 * @var string
+	 */
+	public $release;
+
+	/**
+	 * Time only
+	 *
+	 * @Attribute(name="time-only")
+	 * @var string
+	 */
+	public $timeOnly;
 
     /**
      * @Element
@@ -180,22 +323,5 @@ class Note extends MusicXMLWriter
      * @var NoteheadText
      */
     public $noteheadText;
-    
-    /**
-     * Attack
-     *
-     * @Attribute
-     * @var integer
-     */
-    public $attack;
-
-    /**
-     * Release
-     *
-     * @Attribute
-     * @var integer
-     */
-    public $release;
-    
     
 }
