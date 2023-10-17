71e305055fe16d2a0c9007d180c78706ac49e315
Kamshory, MT <kamshory@yahoo.com>
2023-10-17 10:56:34 +0700

    Update PartTimewise.php

--- inc.lib/classes/MusicXML/Model/PartTimewise.php
+++ inc.lib/classes/MusicXML/Model/PartTimewise.php
@@ -32,9 +32,25 @@ class PartTimewise extends MusicXMLWriter
 
     /** 
      * Elements of part
+     * -
+     * Element of part-timewise consists of
+     * - &lt;note&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/note/
+     * - &lt;backup&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/backup/
+     * - &lt;forward&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/forward/
+     * - &lt;direction&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/direction/
+     * - &lt;attributes&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/attributes/
+     * - &lt;harmony&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/harmony/
+     * - &lt;figured-bass&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/figured-bass/
+     * - &lt;print&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/print/
+     * - &lt;sound&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/sound/
+     * - &lt;listening&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/listening/
+     * - &lt;barline&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/barline/
+     * - &lt;grouping&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/grouping/
+     * - &lt;link&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/link/
+     * - &lt;bookmark&gt; - See https://www.w3.org/2021/06/musicxml40/musicxml-reference/elements/bookmark/
      *
      * @Element(identification="element")
-     * @var MusicXMLWriter[]
+     * @var MusicXMLWriter[] Element of part-timewise
      */
     public $elements;
 }
